e818b42f8b5e05166ab6ebc91da40ef3dd149bd1
GorkyR <1gorky3rojas@gmail.com>
2016-08-02 11:29:14 -0400

    Fix Toogle Behaviour
    With multiple youtube tabs open, if you press MediaPlayPause button,
    playing vids would pause, but paused vids would play. This fix is so
    that it pauses/plays them in sync.
    
    Next step would be to have it so that it only affects the current tab if
    it's Youtube.

--- MultiMKeysSupport/background.js
+++ MultiMKeysSupport/background.js
@@ -1,15 +1,24 @@
 var ports = [];
 
+var playing = true;
+
 function excertcontrol(command, port){
     if (command == "playpause") {
         for (i = 0; i < ports.length; i++){
             try {
-                ports[i].postMessage({"command": "playpause"});
+                if (playing){
+                    ports[i].postMessage({"command": "pause"});
+                }
+                else{
+                    ports[i].postMessage({"command": "play"});
+                }
             }
             catch(err) {
                 ports.splice(i, 1);
             }
         }
+        if (playing){ playing = false; }
+        else{ playing = true; }
     }
     if (command == "prev") {
         for (i = 0; i < ports.length; i++){
